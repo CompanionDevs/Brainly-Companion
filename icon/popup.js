ea0001a643120240650d5bfb1b1243c1543712ad
Luke Graham <62516707+LukeG294@users.noreply.github.com>
2021-10-24 08:30:24 -0700

    Update popup.js

--- icon/popup.js
+++ icon/popup.js
@@ -1,5 +1,5 @@
 const EXTENSION_WEBSTORE_URL = "https://chrome.google.com/webstore/detail/brainly-companion/pkglomajopcdlnknceabnmbbldpegaoi";
-const DISCORD_SERVER_URL = "https://discord.gg/KfwhD3Mbup"; // fails, you should change the link
+const DISCORD_SERVER_URL = "https://discord.gg/Q6AnTauEuU"; 
 
 document.querySelector(".discord-support").onclick = () => window.open(DISCORD_SERVER_URL);
 document.querySelector(".rate-us").onclick = () => window.open(EXTENSION_WEBSTORE_URL);
