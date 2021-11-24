153fe7eb2d26c79b952f69509f84f35ec0656e21
Saransh <67226362+Booperdooper255@users.noreply.github.com>
2021-11-24 11:05:44 +0530

    Update theme.js

--- content-scripts/theme.js
+++ content-scripts/theme.js
@@ -7,3 +7,12 @@ if (localStorage.getItem("--askloadsec") !== null){
 if (localStorage.getItem("--ansbut") !== null){
     document.querySelector(':root').style.setProperty('--ansbut',localStorage.getItem("--ansbut"));
 }
+if (localStorage.getItem("--ansbutelem") !== null){
+    document.querySelector(':root').style.setProperty('--ansbutelem',localStorage.getItem("--ansbutelem"));
+}
+if (localStorage.getItem("--headfootelem") !== null){
+    document.querySelector(':root').style.setProperty('--headfootelem',localStorage.getItem("--headfootelem"));
+}
+if (localStorage.getItem("--secelem") !== null){
+    document.querySelector(':root').style.setProperty('--secelem',localStorage.getItem("--secelem"));
+}
